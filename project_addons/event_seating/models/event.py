486e0d8f7a6fc47f593243ef1e5417f2090b8e1a
Yves Hoyos <yves.hoyos@gmail.com>
2018-10-14 21:39:14 +0200

    Seat selection

--- project_addons/event_seating/models/event.py
+++ project_addons/event_seating/models/event.py
@@ -13,7 +13,7 @@ class EventEvent(models.Model):
     def open_website_set_seats_url(self):
         return {
             'type': 'ir.actions.act_url',
-            'url': '/event_seating/seat_seats/%s' % slug(self),
+            'url': '/event_seating/set_seats/%s' % slug(self),
             'target': 'self',
         }
 
@@ -22,24 +22,80 @@ class EventEvent(models.Model):
         self.ensure_one()
         res = {}
         for registration in self.registration_ids:
-            res[registration.id] = {
-                'id': registration.id,
-                'name': registration.name,
-                'date': registration.date_open,
-                'qty': registration.qty,
-                'seats_count': registration.seats_count,
-                'seats': [],
-            }
+            res[registration.id] = registration.get_registration_data()
         return json.dumps(res)
 
 
 class EventRegistration(models.Model):
     _inherit = 'event.registration'
 
-    seat_ids = fields.Many2many('event.theater.seat', string='Seats')
-    seats_count = fields.Integer(string='Number of seats', compute='_get_seats_count', store=True)
+    seat_ids = fields.One2many('event.registration.seat', 'registration_id', string='Seats')
+    seats_count = fields.Integer(string='Number of seats', compute='_get_seats_count')
 
     @api.one
-    @api.depends('seat_ids')
+    @api.depends('seat_ids', 'seat_ids.registration_id')
     def _get_seats_count(self):
         self.seats_count = len(self.seat_ids)
+
+    @api.multi
+    def get_registration_data(self):
+        self.ensure_one()
+        return  {
+            'id': self.id,
+            'name': self.name,
+            'date': self.date_open,
+            'qty': self.qty,
+            'seats_count': self.seats_count,
+            'seats': [seat.label for seat in self.seat_ids],
+        }
+
+    @api.multi
+    def assign_seats(self, seats_label):
+        self.ensure_one()
+        seats = self.env['event.theater.seat'].search([('theater_id', '=', self.event_id.id), ('label', 'in', seats_label)])
+        if len(seats_label) != len(seats):
+            raise ValidationError(_("Some seats couldn't be found. Please check given labels."))
+        for seat in seats:
+            self.env['event.registration.seat'].create({
+                'registration_id': self.id,
+                'seat_id': seat.id
+            })
+        self.check_seats()
+
+    @api.multi
+    def unassign_seats(self, seats_label):
+        self.ensure_one()
+        seats = self.seat_ids.filtered(lambda r: r.seat_id.label in seats_label)
+        if len(seats_label) != len(seats):
+            raise ValidationError(_("Some seats couldn't be found. Please check given labels."))
+        seats.unlink()
+
+    @api.multi
+    def unassign_all_seats(self):
+        self.ensure_one()
+        self.seat_ids.unlink()
+
+    @api.one
+    @api.constrains('seat_ids')
+    def check_seats(self):
+        if len(self.seat_ids) > self.qty:
+            raise ValidationError(_('There is to much assigned seats than booked seats.'))
+
+
+class EventRegistrationSeat(models.Model):
+    _name = 'event.registration.seat'
+
+    registration_id = fields.Many2one('event.registration', string="Registration", required=True)
+    event_id = fields.Many2one('event.event', string="Event", related='registration_id.event_id', readonly=True, store=True)
+    seat_id = fields.Many2one('event.theater.seat', string='Seat', required=True)
+    label = fields.Char(string='Label', related='seat_id.label', readonly=True, store=True)
+
+    @api.one
+    @api.constrains('registration_id', 'seat_id')
+    def check_disposition_and_names(self):
+        if self.registration_id.event_id.theater_id.id != self.seat_id.theater_id.id:
+            raise ValidationError(_('Seat is not linked to the same theater than the one in the event.'))
+
+    _sql_constraints = [
+        ('unique_seat_event', 'UNIQUE(event_id, seat_id)', 'A seat can only be used once by event'),
+    ]
--- project_addons/event_seating/static/src/js/seating.js
+++ project_addons/event_seating/static/src/js/seating.js
@@ -1,167 +1,270 @@
-var chart, last_selected, selected_seats = [], seat_num = 1;
-var $counter, $cart, $grouped;
+odoo.define('event_seating.seating', function (require) {
+    "use strict";
+    var ajax = require('web.ajax');
+    var core = require('web.core');
+    var _t = core._t;
 
-function group_seats(seats) {
-    if (jQuery.isEmptyObject(seats)) {
-        return [];
-    }
-    var number_label = {};
-    var numbers = [];
-    for (var id in seats) {
-        var label = seats[id];
-        var num = parseInt(label.replace(/\D/g, ""), 10);
-        number_label[num] = label;
-        numbers.push(num);
-    }
-    numbers = numbers.sort(function (a, b) { return a - b; });
-    var groups = [[number_label[numbers[0]]]];
-    var n = 0;
-    for (var i=1; i < numbers.length; i++) {
-        if (numbers[i-1] + 1 == numbers[i]) {
-            groups[n].push(number_label[numbers[i]]);
+    var chart, last_selected, selected_seats = [], seat_num = 1;
+    var $counter, $cart, $grouped;
+
+    function group_seats(seats) {
+        if (jQuery.isEmptyObject(seats)) {
+            return [];
         }
-        else {
-            n++;
-            groups[n] = [number_label[numbers[i]]];
+        var number_label = {};
+        var numbers = [];
+        for (var id in seats) {
+            var label = seats[id];
+            var num = parseInt(label.replace(/\D/g, ""), 10);
+            number_label[num] = label;
+            numbers.push(num);
         }
-    }
-    return groups;
-}
-
-function html_group_seats(seats) {
-    var groups = group_seats(seats);
-    var res = [];
-    var n = 0;
-    for (var i in groups) {
-        if (groups[i].length > 1) {
-            var first = groups[i][0];
-            var last = groups[i].slice(-1)[0];
-            var html = '<span class="label label-success">' + first + '</span> '+
-                       '<i class="fa fa-arrows-h" aria-hidden="true"></i> ' +
-                       '<span class="label label-success">' + last + '</span>';
-            res.push(html);
-        }
-        else if (groups[i].length == 1) {
-            res.push('<span class="label label-success">' + groups[i][0] + '</span>');
-        }
-    }
-    return res.join('<br/>');
-}
-
-function select_seat(seat) {
-    selected_seats[seat.settings.id] = seat.settings.label;
-    $('<span class="label label-primary"/>').text(seat.settings.label)
-        .attr('id', 'cart-item-' + seat.settings.id)
-        .data('seat-id', seat.settings.id)
-        .appendTo($cart);
-    $cart.append(' ');
-}
-
-function unselect_seat(seat) {
-    delete selected_seats[seat.settings.id];
-    $('#cart-item-' + seat.settings.id).remove();
-}
-
-function select_multiple(seat, last_selected_seat, target) {
-    var from = $('.seatCharts-seat').index(seat.node());
-    var to = $('.seatCharts-seat').index(last_selected_seat.node());
-    var start = Math.min(from, to) + 1;
-    var end = Math.max(from, to);
-    $('.seatCharts-seat').slice(start, end).each(function () {
-        var seat = chart.get($(this).attr('id'));
-        if (seat.status() != 'unavailable') {
-            seat.status(target);
-            if (target == 'selected') {
-                select_seat(seat);
+        numbers = numbers.sort(function (a, b) { return a - b; });
+        var groups = [[number_label[numbers[0]]]];
+        var n = 0;
+        for (var i=1; i < numbers.length; i++) {
+            if (numbers[i-1] + 1 == numbers[i]) {
+                groups[n].push(number_label[numbers[i]]);
             }
             else {
-                unselect_seat(seat);
+                n++;
+                groups[n] = [number_label[numbers[i]]];
             }
         }
-    });
-}
-
-function update_counter_and_seats() {
-    $counter.text(Object.keys(selected_seats).length);
-    $grouped.html(html_group_seats(selected_seats));
-}
-
-function load_seating_chart(counter_selector, cart_selector, group_selector) {
-    if (counter_selector) {
-        $counter = $(counter_selector);
-        $cart = $(cart_selector);
-        $grouped = $(group_selector);
+        return groups;
     }
-    var config = {
-        map: map,
-        seats: seats,
-        legend: {
-            node: $('#legend'),
-            items: legend
-        },
-        naming: {
-            top: false,
-            left: true,
-            rows: rows,
-            getId: function(character, row, column) {
-                return row + '-' + seat_num;
-            },
-            getLabel: function (character, row, column) {
-                return row + '-' + seat_num++;
+
+    function html_group_seats(seats) {
+        var groups = group_seats(seats);
+        var res = [];
+        var n = 0;
+        for (var i in groups) {
+            if (groups[i].length > 1) {
+                var first = groups[i][0];
+                var last = groups[i].slice(-1)[0];
+                var html = '<span class="label label-success">' + first + '</span> '+
+                           '<i class="fa fa-arrows-h" aria-hidden="true"></i> ' +
+                           '<span class="label label-success">' + last + '</span>';
+                res.push(html);
+            }
+            else if (groups[i].length == 1) {
+                res.push('<span class="label label-success">' + groups[i][0] + '</span>');
             }
         }
-    };
-    if (counter_selector) {
-        config.click = function (e) {
-            if (last_selected && e && e.shiftKey) {
-                var target = this.status() == 'selected' ? 'available' : 'selected';
-                select_multiple(this, last_selected, target);
-            }
-            last_selected = this;
-            if (this.status() == 'available') {
-                select_seat(this);
-                update_counter_and_seats();
-                return 'selected';
-            }
-            else if (this.status() == 'selected') {
-                unselect_seat(this);
-                update_counter_and_seats();
-                return 'available';
-            }
-            else if (this.status() == 'unavailable') {
-                return 'unavailable';
-            }
-            return this.style();
-        };
-        config.focus = function (e) {
-            if (this.status() == 'available') {
-                $('#seat_informations .seat_number').text(this.settings.label);
-                /*$('#seat_informations .seat_attendee').text(this.settings.label);*/
-                return 'focused';
-            }
-            $('#seat_informations .seat_number').text('');
-            $('#seat_informations .seat_attendee').text('');
-            return this.style();
-        };
+        return res.join('<br/>');
     }
-    chart = $('#seat-map').seatCharts(config);
-    if (counter_selector) {
-        $('.unselect_all_seats').on('click', function () {
-            chart.find('selected').each(function(seatId) {
-                if (this.status() != 'unavailable') {
-                    this.status('available');
+
+    function select_seat(seat) {
+        selected_seats[seat.settings.id] = seat.settings.label;
+        $('<span class="label label-primary"/>').text(seat.settings.label)
+            .attr('id', 'cart-item-' + seat.settings.id)
+            .data('seat-id', seat.settings.id)
+            .appendTo($cart);
+        $cart.append(' ');
+    }
+
+    function unselect_seat(seat) {
+        delete selected_seats[seat.settings.id];
+        $('#cart-item-' + seat.settings.id).remove();
+    }
+
+    function book_seat(seat) {
+        seat.status('unavailable');
+    }
+
+    function unbook_seat(seat) {
+        seat.status('available');
+    }
+
+    function select_multiple(seat, last_selected_seat, target) {
+        var from = $('.seatCharts-seat').index(seat.node());
+        var to = $('.seatCharts-seat').index(last_selected_seat.node());
+        var start = Math.min(from, to) + 1;
+        var end = Math.max(from, to);
+        $('.seatCharts-seat').slice(start, end).each(function () {
+            var seat = chart.get($(this).attr('id'));
+            if (seat.status() != 'unavailable') {
+                seat.status(target);
+                if (target == 'selected') {
+                    select_seat(seat);
                 }
-                unselect_seat(this);
-            });
-            update_counter_and_seats();
-        });
-        $('tr.attendee .select').click(function () {
-            $('#assign').val($(this).closest('tr').data('id'));
-        });
-        $('#validate_assign').click(function () {
-            // Check if selected
-            // Check if attendee
-            // Call ajax
+                else {
+                    unselect_seat(seat);
+                }
+            }
         });
     }
-}
+
+    function book_multiple(seats_label) {
+        for (var i in seats_label) {
+            var seat = chart.get(seats_label[i]);
+            book_seat(seat);
+        }
+    }
+
+    function unbook_multiple(seats_label) {
+        for (var i in seats_label) {
+            var seat = chart.get(seats_label[i]);
+            unbook_seat(seat);
+        }
+    }
+
+    function update_counter_and_seats() {
+        $counter.text(Object.keys(selected_seats).length);
+        $grouped.html(html_group_seats(selected_seats));
+    }
+
+    odoo.load_seating_chart = function(with_click, with_assign, with_table) {
+        $counter = $('#seats_counter');
+        $cart = $('#selected_seats');
+        $grouped = $('#selected_grouped_seats');
+        var config = {
+            map: map,
+            seats: seats,
+            legend: {
+                node: $('#legend'),
+                items: legend
+            },
+            naming: {
+                top: false,
+                left: true,
+                rows: rows,
+                getId: function(character, row, column) {
+                    return row + '-' + seat_num;
+                },
+                getLabel: function (character, row, column) {
+                    return row + '-' + seat_num++;
+                }
+            }
+        };
+        if (with_click) {
+            config.click = function (e) {
+                if (last_selected && e && e.shiftKey) {
+                    var target = this.status() == 'selected' ? 'available' : 'selected';
+                    select_multiple(this, last_selected, target);
+                }
+                last_selected = this;
+                if (this.status() == 'available') {
+                    select_seat(this);
+                    update_counter_and_seats();
+                    return 'selected';
+                }
+                else if (this.status() == 'selected') {
+                    unselect_seat(this);
+                    update_counter_and_seats();
+                    return 'available';
+                }
+                else if (this.status() == 'unavailable') {
+                    return 'unavailable';
+                }
+                return this.style();
+            };
+            config.focus = function (e) {
+                if (this.status() == 'available') {
+                    $('#seat_informations .seat_number').text(this.settings.label);
+                    /*$('#seat_informations .seat_attendee').text(this.settings.label);*/
+                    return 'focused';
+                }
+                $('#seat_informations .seat_number').text('');
+                $('#seat_informations .seat_attendee').text('');
+                return this.style();
+            };
+        }
+        chart = $('#seat-map').seatCharts(config);
+        if (with_click) {
+            $('.unselect_all_seats').on('click', function () {
+                chart.find('selected').each(function(seatId) {
+                    if (this.status() != 'unavailable') {
+                        this.status('available');
+                    }
+                    unselect_seat(this);
+                });
+                selected_seats = [];
+                $cart.html('');
+                $grouped.html('');
+                update_counter_and_seats();
+                $('#assign').val('');
+            });
+        }
+        if (with_table) {
+            if (registrations && !$.isEmptyObject(registrations)) {
+                var booked_seats = [];
+                for (var key in registrations) {
+                    for (var i in registrations[key].seats) {
+                        booked_seats.push(registrations[key].seats[i]);
+                    }
+                }
+                book_multiple(Object.values(booked_seats));
+            }
+            $('tr.attendee .select').click(function () {
+                $('#assign').val($(this).closest('tr').data('id'));
+                if (!$.isEmptyObject(selected_seats)) {
+                    $('#validate_assign').click();
+                }
+            });
+            $('tr.attendee .unassign_all').click(function () {
+                var registration_id = parseInt($(this).closest('tr').data('id'));
+                ajax.jsonRpc('/event_seating/unassign_all_seats', 'call', {
+                    registration_id:  registration_id,
+                }).then(function (result) {
+                    console.log(result);
+                    if (result.success) {
+                        unbook_multiple(Object.values(registrations[registration_id].seats));
+                        registrations[registration_id] = result.registration;
+                        var $table_tr = $('#attendees_list .attendee[data-id='+registration_id+']');
+                        $table_tr.find('.seats_count').text(result.registration.seats_count);
+                        $table_tr.find('.seats_qty').text(result.registration.qty);
+                        $('.unselect_all_seats').click();
+                    }
+                    else {
+                        alert(result.error);
+                    }
+                });
+            });
+        }
+        if (with_assign) {
+            $('#validate_assign').click(function () {
+                // Check if selected
+                if ($.isEmptyObject(selected_seats)) {
+                    alert(_t('Please select seats first.'));
+                    return false;
+                }
+                // Check if attendee
+                var registration_value = $('#assign').val();
+                var registration_id;
+                if (! registration_value) {
+                    alert(_t('Please select attendee first.'));
+                    return false;
+                }
+                try {
+                    registration_id = parseInt(registration_value);
+                }
+                catch (exception) {
+                    alert(_t('Invalid value for the attendee.'));
+                    return false;
+                }
+                if (isNaN(registration_id)) {
+                    alert(_t('Invalid value for the attendee.'));
+                    return false;
+                }
+                // Call ajax
+                ajax.jsonRpc('/event_seating/assign_seats', 'call', {
+                    registration_id: registration_id,
+                    seats: Object.values(selected_seats)
+                }).then(function (result) {
+                    if (result.success) {
+                        registrations[result.registration.id] = result.registration;
+                        book_multiple(Object.values(selected_seats));
+                        var $table_tr = $('#attendees_list .attendee[data-id='+registration_id+']');
+                        $table_tr.find('.seats_count').text(result.registration.seats_count);
+                        $table_tr.find('.seats_qty').text(result.registration.qty);
+                        $('.unselect_all_seats').click();
+                    }
+                    else {
+                        alert(result.error);
+                    }
+                });
+            });
+        }
+    }
+});
